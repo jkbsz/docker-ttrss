035f7a3fccb0a3487b7d3afca7df980484ed5002
Jakub Szczygłowski <jszczyglowski@gmail.com>
2018-07-22 12:29:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# TTRSS Docker Compose
+
+## Description
+
+A way to run Tiny Tiny RSS (https://tt-rss.org/) in a docker container.
+
+Two containers:
+- DB: PostgreSQL
+- App: Apache + PHP + TT-RSS
+
+PostgreSQL data is stored inside a Docker Volume to persist data batween restarts and upgrades.
+Still I would not update before updating the Volume.
+By design needs something in front that will provide encryption.
+
+## Usage
+
+docker-compose up --detach
+
+
+login on http://127.0.0.1/
+user: admin
+password: password
+
+
---
+++ app/ttrss-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -Eeo pipefail
+
+if [[ "$TTRSS_DB_TYPE" != "pgsql" ]] ; then
+	echo "[$(date)] ERROR - pgsql is the only DB type supported by this script"
+	exit 1
+fi
+
+echo "[$(date)] Setting up DB"
+
+echo "[$(date)] Checking if DB is up..."
+until psql --host "$TTRSS_DB_HOST" --port "$TTRSS_DB_PORT" --username "$TTRSS_DB_USER" --dbname "$TTRSS_DB_NAME" --command "select current_timestamp;" ; do
+	>&2 echo "Waiting for postgress..."
+	sleep 3
+done
+
+set -Eeo pipefail
+
+rowcount=$(psql --host "$TTRSS_DB_HOST" --port "$TTRSS_DB_PORT" --username "$TTRSS_DB_USER" --dbname "$TTRSS_DB_NAME" --tuples-only --command "SELECT count(*) FROM information_schema.tables WHERE table_name like 'ttrss_%'" | awk '{print $1}')
+
+echo "[$(date)] ttrss_* tables [$rowcount]"
+
+if [[ "$rowcount" == "0" ]] ; then
+	echo "[$(date)] No ttrss_* tables found - running schema script"
+	psql --host $TTRSS_DB_HOST --port $TTRSS_DB_PORT --username $TTRSS_DB_USER --dbname $TTRSS_DB_NAME --file /var/www/html/schema/ttrss_schema_pgsql.sql
+else
+	echo "[$(date)] Table ttrss_* detected - moving on..."
+fi
+
+echo "[$(date)] Setting up config.php"
+
+cp /var/www/html/config.php-dist /var/www/html/config.php
+sed -ri \
+	-e "s#(define\('DB_HOST').*\$#\1, '$TTRSS_DB_HOST');#g" \
+	-e "s#(define\('DB_TYPE').*\$#\1, '$TTRSS_DB_TYPE');#g" \
+	-e "s#(define\('DB_USER').*\$#\1, '$TTRSS_DB_USER');#g" \
+	-e "s#(define\('DB_NAME').*\$#\1, '$TTRSS_DB_NAME');#g" \
+	-e "s#(define\('DB_PASS').*\$#\1, '$TTRSS_DB_PASSWORD');#g" \
+	-e "s#(define\('DB_PORT').*\$#\1, '$TTRSS_DB_PORT');#g" \
+	-e "s#(define\('SELF_URL_PATH').*\$#\1, '$TTRSS_SELF_URL_PATH');#g" \
+	/var/www/html/config.php
+
+echo "[$(date)] Running Apache and update daemon"
+
+sudo -u www-data /usr/bin/php /var/www/html/update_daemon2.php & /usr/sbin/apache2ctl -D FOREGROUND
+
